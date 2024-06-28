0e2cf1884703862fe9acf49289f843b3f1fe5e22
z1shivam <shivamk.services@gmail.com>
2024-06-29 01:00:31 +0530

    trying to connect drizzle to neon

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,7 +1,7 @@
-export default function Header(){
-    return (
-        <div>
-            <h1>Header</h1>
-        </div>
-    )
+export default function Header() {
+  return (
+    <header className="fixed top-0 z-50 h-14 w-full text-white bg-rose-600">
+      <h1>Header</h1>
+    </header>
+  );
 }
